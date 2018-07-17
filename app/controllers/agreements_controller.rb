ee8a299356665ccba28fc7a269dc3e81d31d52fc
AnnaMaria Wojtyga <annamaria.wojtyga@gmail.com>
2018-07-17 18:05:18 +0200

    call the day off - working still on agreements controller to display dangarous answers in red bg, rest normal

--- app/controllers/agreements_controller.rb
+++ app/controllers/agreements_controller.rb
@@ -24,41 +24,75 @@ class AgreementsController < ApplicationController
     build_and_send_docx
   end
 
+  def all_answers_pool
+    @answers = Answer.all
+  end
+
   def build_and_send_docx
+
     file = "/tmp/" + SecureRandom.uuid + ".docx"
 
     Caracal::Document.save(file) do |docx|
 
-     docx.img 'https://hk2-startup.de/wp-content/uploads/2014/11/HK2_LOGO_300dpi.jpg', width: 100, height: 150
-     4.times do 
-      docx.p
-    end
-    docx.p Date.today.strftime('%B %d, %Y'), color: '666666', align: :right
-    docx.h3 "Kontaktdaten"
-    docx.p "Firma: #{@agreement.comapny}"
-    docx.p "Telefon: #{@agreement.phone}"
-    docx.p "Mail: #{@agreement.mail}"
-    docx.p "Vertrag: #{@agreement.name}"
+      docx.img 'https://hk2-startup.de/wp-content/uploads/2014/11/HK2_LOGO_300dpi.jpg', width: 100, height: 150
+      4.times do 
+        docx.p
+      end
+      docx.p Date.today.strftime('%B %d, %Y'), color: '666666', align: :right
+      docx.h3 "Kontaktdaten"
+      docx.p "Firma: #{@agreement.comapny}"
+      docx.p "Telefon: #{@agreement.phone}"
+      docx.p "Mail: #{@agreement.mail}"
+      docx.p "Vertragtitel: #{@agreement.name}"
+      docx.p "VertragsID: #{@agreement.id}"
 
-    5.times do 
-      docx.p
-    end
+      5.times do 
+        docx.p
+      end
 
-    docx.h2 'Technische und Organisatorische Maßnahmen nach Art. 32 DSGVO'
-    Answer.find(@agreement.answer_ids).each do |a|
-      docx.p a.sentence
-    end
-    # docx.h2 @agreement.answer_ids
-      # raise
-    end
+      docx.hr 
+      docx.p "just for testing"
+      docx.p "#{@agreement.answer_ids}"
+      docx.hr 
+      docx.p 
 
-    UserMailer.report("." + file).deliver_now
+
+      docx.h3 "Bitte beachten:"
+      docx.hr 
+      all_answers_pool # specific answers will be shown in red, cause they are dangarous - we need answers_id that doesnt change
+      all_answers_ids = @answers.ids
+      dangarous_answers = [3, 6] #  manual input of dangarous answers ids. 
+
+      all_answers_ids & dangarous_answers
+
+      Answer.find(@agreement.answer_ids).each do |a| 
+        docx.p a.sentence
+      end
+
+
+      docx.hr
+
+      docx.hr
+      docx.p "just for testing"
+      docx.p "#{@agreement.answer_ids}"
+      docx.hr 
+
+      docx.h2 'Technische und Organisatorische Maßnahmen nach Art. 32 DSGVO'
+      docx.h3 "Verschlüsselung und Pseudonymisierung personenbezogener Daten"
+      raise
+      docx.p 
+    # raise
+    # 
+
   end
 
-  private
+  UserMailer.report("." + file).deliver_now
+end
 
-  def agreement_params
-    params.require(:agreement).permit(:name, :phone, :mail, :comapny)
-  end
+private
+
+def agreement_params
+  params.require(:agreement).permit(:name, :phone, :mail, :comapny, :id)
+end
 
 end
