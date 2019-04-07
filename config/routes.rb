499e00db29933842826ed6da97d47d6540e2ebe9
AnnaMaria Wojtyga <annamaria.wojtyga@gmail.com>
2019-04-08 01:48:42 +0200

    routes changed. FROM: '/agreement_templates/1/agreements/new' TO: root

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :agreements, only: :show
-  resources :agreement_templates do
-    resources :agreements, only: [:new, :create]
-  end
+	resources :agreements, only: :show
+	resources :agreement_templates do
+		resources :agreements, only: [:new, :create]
+	end
 
-  root to: 'pages#home'
+	root to: 'agreements#new'
   # get 'agreements/:id', to: 'agreements#show', as: 'agreement', format: 'docx'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
